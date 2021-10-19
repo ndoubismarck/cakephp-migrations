18a843a6ddec95251f37c81d6b74f1a0b7ed83e8
Bismarck <ndoubismarck@gmail.com>
2021-10-19 10:46:39 +0200

    General update

--- src/View/Helper/MigrationHelper.php
+++ src/View/Helper/MigrationHelper.php
@@ -339,14 +339,8 @@ class MigrationHelper extends Helper
      */
     public function column($tableSchema, $column)
     {
-        $columnType = $tableSchema->getColumnType($column);
-        // Phinx doesn't understand timestampfractional.
-        if ($columnType === 'timestampfractional') {
-            $columnType = 'timestamp';
-        }
-
         return [
-            'columnType' => $columnType,
+            'columnType' => $tableSchema->getColumnType($column),
             'options' => $this->attributes($tableSchema, $column),
         ];
     }
@@ -536,9 +530,9 @@ class MigrationHelper extends Helper
         $join = ', ';
         if ($options['indent']) {
             $join = ',';
-            $start = "\n" . str_repeat('    ', $options['indent']);
+            $start = "\n" . str_repeat("    ", $options['indent']);
             $join .= $start;
-            $end = "\n" . str_repeat('    ', $options['indent'] - 1);
+            $end = "\n" . str_repeat("    ", $options['indent'] - 1);
         }
 
         return $start . implode($join, $list) . ',' . $end;
